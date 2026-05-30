2c562ab4181469ee05d9f0a4805aef6cfc27b891
pasecalouu <ethanph.gontier@gmail.com>
2026-05-30 03:36:43 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,9 +5,8 @@
 // ─────────────────────────────────────────────
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
+ 
 const firebaseConfig = {
   apiKey:            "AIzaSyBTKCzwqVbwVU9qCJMd3S3NuopInaWzgko",
   authDomain:        "inventaire-famille.firebaseapp.com",
@@ -16,9 +15,8 @@ const firebaseConfig = {
   messagingSenderId: "662794095536",
   appId:             "1:662794095536:web:147c4a83d6318bca242475",
 };
-
+ 
 const app       = initializeApp(firebaseConfig);
 export const db       = getFirestore(app);
-export const storage  = getStorage(app);
 export const auth     = getAuth(app);
 export const provider = new GoogleAuthProvider();
